749d246ecaca9a1d799424f6b8b7c2540e35493c
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2024-06-08 22:25:54 +0800

    finished exercise 6

---
+++ Codex Practice/JavaScript Codex/The Console/let&const.js
@@ -0,0 +1,13 @@
+let num1 = 11;
+let num2 = 24;
+let num3 = 22;
+const num4 = 36;
+const num5 = 78;
+
+num3 = 69;
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
