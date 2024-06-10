e31c5cbfe845d998ae3477e975dfbc9ad9201506
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2024-06-10 23:28:58 +0800

    finished exercise 10

--- Codex Practice/JavaScript Codex/Variables/currency.js
+++ Codex Practice/JavaScript Codex/Variables/currency.js
@@ -2,6 +2,20 @@ let chineseYuan = 560;
 let japaneseYen = 2455;
 let southkorenWon  = 3280;
 
-let yuanTOdollar = chineseYuan * 0.15;
+let yuanTOdollar = chineseYuan * 0.14;
 
-console.log(yuanTOdollar);
+let yenTOdollar = japaneseYen * 0.0072;
+
+let wonTOdollar = southkorenWon * 0.00076;
+
+const totalUSD = yuanTOdollar + yenTOdollar + wonTOdollar;
+
+console.log("What do you have left in Yuan? " + chineseYuan);
+console.log("What do you have left in Yen? " + japaneseYen);
+console.log("What do you have left in Won? " + southkorenWon);
+console.log("                                          ");
+console.log("Yuan to Dollar: " + yuanTOdollar);
+console.log("Yen to Dollar: " + yenTOdollar);
+console.log("Won to Dollar: " + wonTOdollar);
+console.log("                                          ");
+console.log("Total amount: " + totalUSD);
