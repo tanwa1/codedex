f525fde9e8197e0864aff653778e07476b973353
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2024-06-11 22:49:54 +0800

    finished exercise 15

--- Codex Practice/JavaScript Codex/Conditionals/magic8Ball.js
+++ Codex Practice/JavaScript Codex/Conditionals/magic8Ball.js
@@ -1,5 +1,33 @@
-console.log("Is Codédex better than Udemy yet?");
+console.log("Question: Is Codédex better than Udemy yet?");
 
-let random = Math.floor(Math.random() * 10);
+let randomNumber = Math.floor(Math.random() * 10);
 
-console.logu(random);
+if (random == 1) {
+    console.log("Magical 8 Ball: Yes - Definitely");
+} else if (random === 2){
+    console.log("Magical 8 Ball: It is decidely so/");
+}
+else if (random === 3){
+    console.log("Magical 8 Ball: Without a doubt.");
+}
+else if (random === 4){
+    console.log("Magical 8 Ball: Reply hazy, try again.");
+}
+else if (random === 5){
+    console.log("Magical 8 Ball: Ask again later.");
+}
+else if (random === 6){
+    console.log("Magical 8 Ball: Better not tell you now.");
+}
+else if (random === 7){
+    console.log("Magical 8 Ball: My sources say no.");
+}
+else if (random === 8){
+    console.log("Magical 8 Ball: Outlook not so good");
+}
+else if (random === 9){
+    console.log("Magical 8 Ball: Very doubtful");
+}
+else {
+    console.log("Magical 8 Ball: Error");
+}
