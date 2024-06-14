1979b6d79755137820095e261ea42616eac31e61
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2024-06-15 01:50:53 +0800

    finished exercise 19

--- Codex Practice/JavaScript Codex/Loops/luckyNumber.js
+++ Codex Practice/JavaScript Codex/Loops/luckyNumber.js
@@ -1,7 +1,7 @@
 const luckyNumber = 10;
-let guess = Math.floor(Math.random() * 10);
-while (guess != 10) {
-    guess = Math.floor(Math.random() * 10);
+let guess = Math.floor(Math.random() * 10) + 1;
+while (guess != luckyNumber) {
     console.log("My lucky number is not: " + guess);
+    guess = Math.floor(Math.random() * 10) + 1;
 }
-console.log("My lucky number is" + guess);
+console.log("My lucky number is " + guess);
