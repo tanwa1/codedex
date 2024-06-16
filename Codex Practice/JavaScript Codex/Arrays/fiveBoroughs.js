1540f7b657617f3d9946e52cf49bc65521b34601
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2024-06-16 20:34:01 +0800

    finished exercise 25

---
+++ Codex Practice/JavaScript Codex/Arrays/fiveBoroughs.js
@@ -0,0 +1,9 @@
+const myNYCDestinations = [
+  "🗽 Manhattan - Washington Square Park",
+  "🏟️ The Bronx - Yankee Stadium",
+  "🎡 Brooklyn - Coney Island",
+  "✈️ Queens - Astoria Park",
+  "🌉 Staten Island - Historic Richmond Town",
+];
+
+console.log(myNYCDestinations);
