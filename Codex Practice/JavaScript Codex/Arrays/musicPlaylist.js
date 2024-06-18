130d8f66c209086525f3663d6c512bd8c864c039
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2024-06-19 02:46:14 +0800

    finished exercise 26

---
+++ Codex Practice/JavaScript Codex/Arrays/musicPlaylist.js
@@ -0,0 +1,16 @@
+const musicPlaylist = [
+    "Dry Flower",
+    "Spring and Winter",
+    "Sabotage",
+    "I Wanna Dance With Somebody",
+    "Don't Speak",
+    "Bulls On Parade",
+    "Thriller",
+    "The Breaks",
+    "Brick",
+    "Aeroplane Over the Sea",
+    "Fish"
+  ];
+
+
+  console.log(musicPlaylist);
