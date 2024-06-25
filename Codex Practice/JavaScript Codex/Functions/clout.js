3cc087b920c9dce555908d1783f43b39a00ac468
tanwa <73098650+tanwa1@users.noreply.github.com>
2024-06-25 16:42:07 +0800

    Update clout.js

--- Codex Practice/JavaScript Codex/Functions/clout.js
+++ Codex Practice/JavaScript Codex/Functions/clout.js
@@ -2,8 +2,8 @@ const recentTikTokViews = [1932, 2300, 453, 5222, 6733, 7402, 8334];
 const recentInstagramViews = [936, 2576, 453, 7013, 5489, 7402, 3921];
 const recentYouTubeViews = [2300, 453, 5222, 989, 6733, 7402, 2789];
 
+function mean(viewsArrays) { 
 let totalViews = 0;
-function mean(viewsArrays) {
   for (let i = 0; i < viewsArrays.length; i++) {
     totalViews += viewsArrays[i];
   }
