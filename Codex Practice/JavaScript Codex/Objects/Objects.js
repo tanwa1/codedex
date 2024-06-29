927b0a5ff8ca19bf4019917c321d65de90cf53a5
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2024-06-29 20:49:51 +0800

    finished exercise 34

---
+++ Codex Practice/JavaScript Codex/Objects/Objects.js
@@ -0,0 +1,13 @@
+const pizzaTopping = "Cheese";
+const pizzaType = "Pan";
+const pizzaSlices = 8;
+const pizzaPrice = 12.99;
+
+const pizza = {
+  topping: "Pepperoni",
+  type: "Hand-tossed",
+  sliecs: 12,
+  prices: 14.99,
+};
+
+console.log(pizza);
