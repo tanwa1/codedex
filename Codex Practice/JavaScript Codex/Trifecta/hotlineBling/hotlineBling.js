7da5db1fdaa25f8cc01ad356b0c2921838466a5a
tanwa1 <73098650+tanwa1@users.noreply.github.com>
2024-07-12 03:59:46 +0800

    finished exercise 41

---
+++ Codex Practice/JavaScript Codex/Trifecta/hotlineBling/hotlineBling.js
@@ -0,0 +1,10 @@
+// function changeTextandImage() {
+let drakePicTwo = (document.getElementById("drake-pic-2").src =
+  "https://i.imgur.com/RGbh6zY.png");
+
+let heading = (document.getElementById("heading-2").innerText =
+  "Learning by Doing!");
+
+console.log(drakePicTwo);
+console.log(heading);
+// }
