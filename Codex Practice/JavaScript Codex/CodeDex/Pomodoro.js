7da489480f83826e79697e8f3f0611b3e7660f50
tanwa1 <duanyotanwa@gmail.com>
2024-10-04 05:03:18 +0800

    added pomodoro timer

--- Codex Practice/JavaScript Codex/CodeDex/Pomodoro.js
+++ Codex Practice/JavaScript Codex/CodeDex/Pomodoro.js
@@ -1,52 +1,71 @@
-const bells = new Audio('./sounds/bell.wav'); 
+const bells = new Audio('Alarm.mp3'); 
 const startBtn = document.querySelector('.btn-start'); 
+const resetBtn = document.querySelector('.btn-reset');
+const pauseBtn = document.querySelector('.btn-pause'); 
 const session = document.querySelector('.minutes'); 
+const secondDiv = document.querySelector('.seconds');
 let myInterval; 
 let state = true;
+let isPaused = false;
+let totalSeconds;
 
 const appTimer = () => {
     const sessionAmount = Number.parseInt(session.textContent)
   
-    if(state) {
+    if(state || isPaused) {
       state = false;
-      let totalSeconds = sessionAmount * 60;
+      isPaused = false;
+      if (!totalSeconds) {
+        totalSeconds = sessionAmount * 60;
+      }
       
       const updateSeconds = () => {
-        const minuteDiv = document.querySelector('.minutes');
-        const secondDiv = document.querySelector('.seconds');
-      
-        totalSeconds--;
-      
-        let minutesLeft = Math.floor(totalSeconds/60);
-        let secondsLeft = totalSeconds % 60;
-      
-        if(secondsLeft < 10) {
-          secondDiv.textContent = '0' + secondsLeft;
-        } else {
-          secondDiv.textContent = secondsLeft;
-        }
-        minuteDiv.textContent = `${minutesLeft}`
-      
-        if(minutesLeft === 0 && secondsLeft === 0) {
-          bells.play()
-          clearInterval(myInterval);
+        if (!isPaused) {
+          totalSeconds--;
+        
+          let minutesLeft = Math.floor(totalSeconds/60);
+          let secondsLeft = totalSeconds % 60;
+        
+          if(secondsLeft < 10) {
+            secondDiv.textContent = '0' + secondsLeft;
+          } else {
+            secondDiv.textContent = secondsLeft;
+          }
+          session.textContent = minutesLeft;
+        
+          if(totalSeconds <= 0) {
+            bells.play()
+            clearInterval(myInterval);
+            state = true;
+            isPaused = false;
+          }
         }
       }
-      
       myInterval = setInterval(updateSeconds, 1000);
-    } else {
+    } 
+    else {
       alert('Session has already started.')
     }
-    if(secondsLeft < 10) {
-        secondDiv.textContent = '0' + secondsLeft;
-      } else {
-        secondDiv.textContent = secondsLeft;
-      }
-      minuteDiv.textContent = `${minutesLeft}`;
-      
-      if(minutesLeft === 0 && secondsLeft === 0) {
-        bells.play();
-        clearInterval(myInterval);
-      }
   }
+
+  const resetTimer = () => {
+    clearInterval(myInterval)
+    state = true;
+    isPaused = false;
+    bells.pause();
+    bells.currentTime = 0;
+    session.textContent = '25';
+    secondDiv.textContent = '00';
+  }
+
+  const pauseTimer = () => {
+    if (!state) {
+      isPaused = !isPaused;
+    }
+    bells.pause();
+    bells.currentTime = 0;  
+  }
+
   startBtn.addEventListener('click', appTimer);
+  resetBtn.addEventListener('click', resetTimer);
+  pauseBtn.addEventListener('click', pauseTimer);
